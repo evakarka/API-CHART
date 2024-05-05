e0aa2122a65f8edf5bfaf134b6e31d9752c21992
Eva <126707187+evakarka@users.noreply.github.com>
2024-05-05 22:50:34 +0300

    forgot password

--- forgot-password.php
+++ forgot-password.php
@@ -9,35 +9,40 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+    <title>Forgot Password</title>
 </head>
 <body>
+    <div class="nav">
+        <div class="logo">
+            <p><a href="home.php">Logo</a></p>
+        </div>
+
+        <div class="right-links">
+            <a href="#">Change Profile</a>
+            <a href="logout.php"> <button class="btn">Log Out</button></a>
+        </div>
+    </div>
+
+
     <div class="container">
         <div class="box form-box">
-            <header>Login</header>
-            <form action="" method="post">
+            <header>Forgot Password</header>
+            <form action="send-password-reset.php" method="post">
 
                 <div class="field input">
                     <label for="username">Username</label>
-                    <input type="text" name="username" id="username" required>
+                    <input type="text" name="username" id="username" autocomplete="off" required>
                 </div>
     
-                <div class="field input">
-                    <label for="username">Password</label>
-                    <input type="password" name="password" id="password" required>
-                </div>
-
-                <a href="forgot-password.php">Forgot password?</a>
     
                 <div class="field">
                     <input type="submit" class="btn" name="submit" value="Login" required>
                 </div>
-                <div class="links">
-                    Don't have account? <a href="register.php">Sign Up Now</a>
-                </div>
 
-            </form>
+
+            </form>          
         </div>
     </div>
+
 </body>
 </html>
