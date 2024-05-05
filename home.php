a9fe16539deb0b72cbf4fd6ef199ccd02fc907cf
Eva <126707187+evakarka@users.noreply.github.com>
2024-05-06 00:42:49 +0300

    Update home.php

--- home.php
+++ home.php
@@ -1,11 +1,23 @@
+<?php
+session_start();
+
+include("php/config.php");
+if (!isset($_SESSION['valid'])) {
+    header("Location: index.php");
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <title>Home</title>
 </head>
+
 <body>
     <div class="nav">
         <div class="logo">
@@ -13,6 +25,28 @@
         </div>
 
         <div class="right-links">
+
+            <?php
+
+            $id = $_SESSION['id'];
+
+            // Ελέγχουμε εάν η τιμή του $id είναι διαθέσιμη και έγκυρη
+            if (!empty($id) && is_numeric($id)) {
+                $query = mysqli_query($con, "SELECT * FROM users WHERE Id=$id");
+
+                while ($result = mysqli_fetch_assoc($query)) {
+                    $res_Uname = $result['Useraname'];
+                    $res_Email = $result['Email'];
+                    $res_Age = $result['Age'];
+                    $res_id = $result['Id'];
+                }
+
+                echo "<a href='edit.php?Id=$res_id'>Change Profile</a>";
+            } else {
+                echo "Invalid user ID";
+            }
+            ?>
+
             <a href="#">Change Profile</a>
             <a href="logout.php"> <button class="btn">Log Out</button></a>
         </div>
@@ -38,4 +72,5 @@
 
     </main>
 </body>
+
 </html>
