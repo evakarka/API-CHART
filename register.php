12739200d99e13522463491233c6e266eb373c78
Eva <126707187+evakarka@users.noreply.github.com>
2024-05-05 22:35:42 +0300

    html to php

--- register.php
+++ register.php
@@ -2,27 +2,20 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge"> -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
     <link rel="stylesheet" href="style.css">
-    <title>Change Profile</title>
+    <title>Register</title>
 </head>
 <body>
-    <div class="nav">
-        <div class="logo">
-            <p>Logo</p>
-        </div>
-
-        <div class="right-links">
-            <a href="#">Change Profile</a>
-            <a href="logout.php"> <button class="btn">Log Out</button></a>
-        </div>
-    </div>
-
-
     <div class="container">
         <div class="box form-box">
-            <header>Change Profile</header>
+            <header>Sign Up</header>
             <form action="" method="post">
 
                 <div class="field input">
@@ -40,15 +33,19 @@
                     <input type="number" name="username" id="username" autocomplete="off" required>
                 </div>
     
+                <div class="field input">
+                    <label for="username">Password</label>
+                    <input type="password" name="password" id="password" autocomplete="off" required>
+                </div>
     
                 <div class="field">
                     <input type="submit" class="btn" name="submit" value="Login" required>
                 </div>
-                
-            </form>
+                <div class="links">
+                    Already a member? <a href="index.php">Sign In</a>
+                </div>
 
-            
-            
+            </form>          
         </div>
     </div>
 </body>
