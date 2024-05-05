66cad866936674d07dc2368e486b180f36264c7d
Eva <126707187+evakarka@users.noreply.github.com>
2024-05-06 00:29:30 +0300

    update index and error messages

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,12 +18,42 @@
 <body>
     <div class="container">
         <div class="box form-box">
+
+            <?php
+            include("php/config.php");
+
+            if(isset($_POST['submit'])){
+                $email = mysqli_real_escape_string($con,$_POST['email']);
+                $password = mysqli_real_escape_string($con,$_POST['password']);
+            
+                $result = mysqli_query($con,"SELECT * FROM users WHERE Email='$email' AND Password='$password' ") or die("Select Error");
+                $row = mysqli_fetch_assoc($result);
+            
+                if(is_array($row) && !empty($row)){
+                    $_SESSION['valid'] = $row['Email'];
+                    $_SESSION['username'] = $row['Username'];
+                    $_SESSION['age'] = $row['Age'];
+                    $_SESSION['id'] = $row['Id'];
+            
+                    header("Location: home.php");
+                    exit; // Σταματάει την εκτέλεση 
+                } else {
+                    echo "<div class='message'>
+                    <p>Wrong Email or Password</p>
+                    </div> <br>";
+                    echo "<a href='index.php'><button class='btn'>Go Back</button>";
+                }
+            }else {
+
+
+            ?>
+
             <header>Login</header>
             <form action="" method="post">
 
                 <div class="field input">
-                    <label for="username">Username</label>
-                    <input type="text" name="username" id="username" required>
+                    <label for="email">Email</label>
+                    <input type="text" name="email" id="email" required>
                 </div>
     
                 <div class="field input">
@@ -38,6 +72,7 @@
 
             </form>
         </div>
+        <?php } ?>
     </div>
 </body>
 </html>
