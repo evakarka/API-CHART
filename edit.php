c507a7ceb9a785d0243389117e2d027165f9d73f
Eva <126707187+evakarka@users.noreply.github.com>
2024-05-06 01:52:19 +0300

    edit and home

--- edit.php
+++ edit.php
@@ -1,3 +1,13 @@
+<?php
+    session_start();
+
+    include("php/config.php");
+    if (!isset($_SESSION['valid'])) {
+    header("Location: index.php");
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -22,22 +32,52 @@
 
     <div class="container">
         <div class="box form-box">
+
+            <?php
+
+            if(isset($_POST['submit'])){
+                $username = $_POST['username'];
+                $email = $_POST['email'];
+                $age = $_POST['age'];
+
+                $id = $_SESSION['id'];
+
+                $edit_query = mysqli_query($con, "UPDATE users SET Username='$username', Email='$email', Age='$age' WHERE id=$id ") or die("Error Occurred");
+
+                if($edit_query){
+                    echo "<div class='message'
+                    <p>Profile Updated!</p>
+                    </div> <br>";
+                    echo "<a href='home.php'><button class='btn'>Go Home</button>";
+                }
+            }else{
+
+                $id = $_SESSION['id'];
+                $query = mysqli_query($con,"SELECT*FROM users WHERE Id=$id");
+
+                while($result = mysqli_fetch_assoc($query)){
+                    $res_Uname = $result['Username'];
+                    $res_Email = $result['Email'];
+                    $res_Age = $result['Age'];
+                }
+            ?>
+
             <header>Change Profile</header>
             <form action="" method="post">
 
                 <div class="field input">
                     <label for="username">Username</label>
-                    <input type="text" name="username" id="username" autocomplete="off" required>
+                    <input type="text" name="username" id="username" value="<?php echo $res_Uname; ?>" autocomplete="off" required>
                 </div>
     
                 <div class="field input">
                     <label for="email">Email</label>
-                    <input type="text" name="username" id="username" autocomplete="off" required>
+                    <input type="text" name="email" id="email" autocomplete="off" value="<?php echo $res_Email; ?>" required>
                 </div>
     
                 <div class="field input">
                     <label for="age">Age</label>
-                    <input type="number" name="username" id="username" autocomplete="off" required>
+                    <input type="number" name="age" id="age" autocomplete="off" value="<?php echo $res_Age; ?>" required>
                 </div>
     
     
@@ -47,6 +87,7 @@
 
             </form>
         </div>
+        <?php } ?>
     </div>
 </body>
 </html>
