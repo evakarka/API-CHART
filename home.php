bdac18e69d7651180fe662f198b1caf87dfd948c
Eva <126707187+evakarka@users.noreply.github.com>
2024-05-07 01:23:00 +0300

    allages

--- home.php
+++ home.php
@@ -1,65 +1,97 @@
-<?php 
-   session_start();
+<?php
+session_start();
 
-   include("php/config.php");
-   if(!isset($_SESSION['valid'])){
+include("php/config.php");
+if (!isset($_SESSION['valid'])) {
     header("Location: index.php");
-   }
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style/style.css">
-    <title>Home</title>
+    <title>Επιστημονικά Περιοδικά για την Υγεία</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body>
+
+<body class="bg-light">
     <div class="nav">
         <div class="logo">
-            <p><a href="home.php">Logo</a> </p>
+            <p><a href="home.php">Logo</a></p>
         </div>
 
         <div class="right-links">
+            <?php
 
-            <?php 
-            
             $id = $_SESSION['id'];
-            $query = mysqli_query($con,"SELECT*FROM users WHERE Id=$id");
 
-            while($result = mysqli_fetch_assoc($query)){
-                $res_Uname = $result['Username'];
-                $res_Email = $result['Email'];
-                $res_Age = $result['Age'];
-                $res_id = $result['Id'];
+            // Ελέγχουμε εάν η τιμή του $id είναι διαθέσιμη και έγκυρη
+            if (!empty($id) && is_numeric($id)) {
+                $query = mysqli_query($con, "SELECT * FROM users WHERE Id=$id");
+
+                while ($result = mysqli_fetch_assoc($query)) {
+                    $res_Uname = $result['Useraname'];
+                    $res_Email = $result['Email'];
+                    $res_Age = $result['Age'];
+                    $res_id = $result['Id'];
+                }
+
+                echo "<a href='edit.php?Id=$res_id'>Change Profile</a>";
+            } else {
+                echo "Invalid user ID";
             }
-            
-            echo "<a href='edit.php?Id=$res_id'>Change Profile</a>";
             ?>
 
-            <a href="php/logout.php"> <button class="btn">Log Out</button> </a>
-
+            <a href="#">Change Profile</a>
+            <a href="php/logout.php"> <button class="btn">Log Out</button></a>
         </div>
     </div>
-    <main>
 
-       <div class="main-box top">
-          <div class="top">
-            <div class="box">
-                <p>Hello <b><?php echo $res_Uname ?></b>, Welcome</p>
-            </div>
-            <div class="box">
-                <p>Your email is <b><?php echo $res_Email ?></b>.</p>
-            </div>
-          </div>
-          <div class="bottom">
-            <div class="box">
-                <p>And you are <b><?php echo $res_Age ?> years old</b>.</p> 
-            </div>
-          </div>
-       </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-8 offset-md-3">
+                <h2 class="text-center mt-4 mb-4">Scientific Journals on COVID-19 Global Data</h2>
+                <div class="input-group mb-3">
+                    <p>
+                    This application calls upon WHO COVID-19 global data published in 
+                        <a href="https://covid19.who.int">World Health Organization (WHO).</a>
+                    </p>
+                    <p>
+                    To utilize it, you need to log in to the platform.
+                    </p>
+                </div>
 
-    </main>
+                <div class="input-group mb-3">
+                    <button class="btn btn-primary" type="button">Login</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer class="container-fluid text-center">
+        <p>Copyright © 2024 - Eva Karka</p>
+    </footer>
+    <script src="script.js"></script>
+
+    <!-- Ενσωματώνουμε τον κώδικα JavaScript για το Autocomplete -->
+    <script>
+        $(function() {
+            // Ορίζουμε τις διαθέσιμες επιλογές
+            var availableCategories = [
+                "Pharmacy",
+                "Health",
+                "Hospital"
+            ];
+            // Εφαρμόζουμε το autocomplete στο πεδίο εισαγωγής με id "categoryInput"
+            $("#categoryInput").autocomplete({
+                source: availableCategories
+            });
+        });
+    </script>
 </body>
+
 </html>
