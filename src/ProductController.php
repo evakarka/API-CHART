a4b77e6dc67497ec72272da6b6fa9ae81b781005
Eva <126707187+evakarka@users.noreply.github.com>
2024-05-07 02:57:50 +0300

    update

--- src/ProductController.php
+++ src/ProductController.php
@@ -28,8 +28,19 @@ class ProductController
     private function processCollectionRequest(string $method): void
     {
         switch ($method) {
-            case: "GET":
+            case "GET":
                 echo json_encode($this->gateway->getAll());
+                break;
+
+            case "POST":
+                $data = (array) json_decode(file_get_contents("php://input"), true);
+
+                $id = $this->gateway->create($data);
+
+                echo json_encode([
+                    "message" => "Product created",
+                    "id" => $id
+                ]);
                 break;
         }
     }
--- src/ProductGateway.php
+++ src/ProductGateway.php
@@ -27,4 +27,20 @@ class ProductGateway
 
         return $data;
     }
+
+    public function create(array $data): string
+    {
+        $sql = "INSERT INTO product (name, size, is_available)
+                VALUES (:name, :size, :is_available)";
+
+        $stmt = $this->conn->prepare($sql);
+
+        $stmt->bindValue(":name", $data["name"], PDO::PARAM_STR);
+        $stmt->bindValue(":size", $data["size"] ?? 0, PDO::PARAM_INT);
+        $stmt->bindValue(":is_available", (bool) $data["is_available"] ?? false, PDO::PARAM_BOOL);
+
+        $stmt->execute();
+
+        return $this->conn->lastInsertId();
+    }
 }
