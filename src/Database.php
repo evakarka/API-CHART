c081a059819422f24b924e2bc75ea9b0b53ed55f
Eva <126707187+evakarka@users.noreply.github.com>
2024-05-07 02:12:58 +0300

    update

---
+++ src/Database.php
@@ -0,0 +1,17 @@
+<?php
+
+class Database
+{
+    public function _construct(private string $host,
+                               private string $name,
+                               private string $user,
+                               private string $password)
+    {}
+
+    public function getConnection(): PDO
+    {
+        $dns = "mysql:host={$this->host};dbname={$this->name};charset=utf8";
+
+        new PDO($dsn, $this->user, $this->password);
+    }
+}
