3dc39a6286cf487e534ba14d9e5fdc1ab6765793
Eva <126707187+evakarka@users.noreply.github.com>
2024-05-13 01:46:34 +0300

    Update index.php

--- front-end/index.php
+++ front-end/index.php
@@ -18,7 +18,7 @@ if (isset($_SESSION["user_id"])) {
 <!DOCTYPE html>
 <html>
 <head>
-    <title>COVID-19 Journo</title>
+    <title>DailyNews</title>
     <meta charset="UTF-8">
     <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css"> -->
     <link rel="stylesheet" href="style/style.css">
@@ -28,12 +28,10 @@ if (isset($_SESSION["user_id"])) {
 <body class="bg-light">
     <div class="nav">
         <div class="logo">
-            <p><a href="home.html">COVID-19 Journo</a></p>
+            <p><a href="index.php">DailyNews</a></p>
         </div>
 
         <div class="right-links">
-            <!-- <a class="btn btn-outline-primary btn-sm" href="profile.php">Profile</a> -->
-            <a class="btn btn-outline-primary btn-sm" href="logout.php">Logout</a>
         </div>
     </div>
 
@@ -41,12 +39,12 @@ if (isset($_SESSION["user_id"])) {
     <div class="container">
         <div class="row">
             <div class="col-sm-8 offset-md-3">
-                <h2 class="text-center mt-4 mb-4">Scientific Journals on COVID-19 Global Data</h2>
+                <h2 class="text-center mt-4 mb-4">Scientific journals on daily minimum temperatures data analysis</h2>
                 <br>
                 <div class="input-group mb-3">
                     <p>
-                    This application calls upon WHO COVID-19 global data published in 
-                        <a href="https://covid19.who.int">World Health Organization (WHO).</a>
+                    This application calls upon global data published in  
+                        <a href="https://catalog.data.gov/dataset/?res_format=CSV">DATA.GOV</a>
                     </p>
                     <p>
                     To utilize it, you need to log in to the platform.
