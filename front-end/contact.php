c912f1052785f169ac28021763f28ccee939dbc4
Eva <126707187+evakarka@users.noreply.github.com>
2024-05-14 02:15:03 +0300

    update

--- front-end/contact.php
+++ front-end/contact.php
@@ -21,7 +21,7 @@ if (isset($_SESSION["user_id"])) {
 <head>
 <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DailyNews</title>
+    <title>DailyNewsChart - Contact</title>
     <link rel="icon" href="/img/logo.png" type="image/x-icon">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
